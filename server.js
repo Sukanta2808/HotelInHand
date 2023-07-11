fec2aa55e02603df05bcaee5bdd48377a51ce233
Sukanta Mallick <sm7688744@gmail.com>
2023-07-12 01:49:47 +0530

    cors updated

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors({ credentials: true, origin: 'https://hotelinhand.netlify.app/' }));
 app.use(express.static('public'));
 
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomsRoute');
