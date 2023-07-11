7952f1056240bc201336cc92667a3b00df2ae8a3
Sukanta Mallick <sm7688744@gmail.com>
2023-07-12 01:26:01 +0530

    cors updated again

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors({ credentials: true, origin: `${FRONTEND_URL}` }));
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomsRoute');
 const usersRoute=require('./routes/usersRoute');
