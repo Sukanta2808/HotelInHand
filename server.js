0d01759a64bd116b1e3d029b8b756785ef14d63f
Sukanta Mallick <sm7688744@gmail.com>
2023-07-12 01:15:12 +0530

    cors updated

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app=express();
+
+const FRONTEND_URL='hotelinhand.netlify.app'
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+app.use(cors({ credentials: true, origin: `${FRONTEND_URL}` }));
 app.use(express.static('public'));
 
 const dbConfig=require('./db');
