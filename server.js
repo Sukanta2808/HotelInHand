6c8b352ac670bc9a0f097ce5e65239970e051da2
Sukanta Mallick <sm7688744@gmail.com>
2023-07-12 03:24:50 +0530

    netlify

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cors({ credentials: true, origin: 'https://hotelinhand.netlify.app/' }));
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+  }
+  app.use(cors(corsOptions))
+// app.use(cors({ credentials: true, origin: 'https://hotelinhand.netlify.app/' }));
 app.use(express.static('public'));
+app.set('trust proxy', 1);
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//       next();
+//     });
 
 const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomsRoute');
