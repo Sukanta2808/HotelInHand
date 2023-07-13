8d180411d64891a11ee56d045dbae79112e8c6b3
Sukanta Mallick <sm7688744@gmail.com>
2023-07-13 22:03:44 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const cors = require('cors');
+const path = require("path");
 require('dotenv').config();
 
 const app=express();
@@ -12,8 +13,22 @@ const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200
-  }
-  app.use(cors(corsOptions))
+}
+app.use(cors(corsOptions))
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomsRoute');
