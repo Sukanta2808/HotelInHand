3afe62669cf30dbd0f754917a7cf6b22527c5f15
Sukanta Mallick <sm7688744@gmail.com>
2023-07-16 15:41:36 +0530

    backend

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 const dbConfig=require('./db');
 const roomsRoute=require('./routes/roomsRoute');
