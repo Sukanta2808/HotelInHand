8575576cd6c2ae0d106118192f1cef2f5c780b15
Sukanta2808 <123719194+Sukanta2808@users.noreply.github.com>
2023-12-28 11:58:32 +0530

    Update roomsRoute.js

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -6,7 +6,7 @@ const Room=require('../models/room');
 router.get('/getallrooms',async(req,res)=>{
     try {
         const rooms=await Room.find();
-        res.send(rooms);
+        res.json(rooms);
     } catch (error) {
         console.log(err);
     }
